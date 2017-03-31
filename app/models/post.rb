27cc0667482776c81b6fc988cd8056e73c1d7986
moshiko1988 <moshiko1988@gmail.com>
2017-03-30 21:41:48 -0400

    Add validations
    Add validations to comment posts and users model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :title, length: { maximum: 60 }
+  validates :body, length: { maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   include Authentication
   has_many :comments
   has_many :posts
+  validates :full_name, presence: true
 end
